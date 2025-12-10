7e1fa201852073e213a0d974fd8c72facd1872ee
brockeh8 <bfreiberger1@student.gsu.edu>
2025-12-10 14:29:10 -0500

    Authentication shell and file clean up

---
+++ lib/screens/auth_shell.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'groups/groups_list_screen.dart';
+import 'rooms/rooms_list_screen.dart';
+import 'profile/profile_screen.dart';
+
+class AppShell extends StatefulWidget {
+  const AppShell({super.key});
+
+  @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  int _index = 0;
+
+  final _tabs = const [
+    GroupsListScreen(),
+    RoomsListScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _tabs[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.groups), label: "Groups"),
+          BottomNavigationBarItem(icon: Icon(Icons.meeting_room), label: "Rooms"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
